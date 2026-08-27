b53704811824de6026edd400332e63f9bf3d996e
Nigel Price <nigelprice3000@gmail.com>
2026-08-27 15:46:44 -0400

    Restore image preloading functionality
    Restores the window.onload function and reintroduces the preloadImages functionality for hover images.

--- script.js
+++ script.js
@@ -2,37 +2,3 @@ window.onload = function() {
     document.body.className += " loaded";
 }
 
-const hoverImages = [
-    // Home Page Hover Images
-    'Home_EngineeringHoverNEW.png',
-    'Home_VFXHoverNEW.png',
-    'Home_ExtWorkHoverNEW.png',
-    'Home_CareerHoverNEW.png',
-    
-    // Engineering Projects Hover Images
-    'FFBWheelFrontRightHoverShrankNEW.png',
-    'FFBWheelTopDownHoverShrankNEW.png',
-    
-    // 3D Modeling Hover Images
-    'StageV2ShowcaseHoverNEW.png',
-    'SilversRoomShowcaseHover.png',
-    'BuffetAltView.png',
-    'WinkerAnimLonger.gif',
-    'LPCtopdown.jpg',
-    'StageV1ShowcaseHover.gif',
-    
-    // External Work Hover Images
-    'RUCRT_SiteShowcaseHoverNEW.png'
-];
-
-function preloadImages(urls) {
-    urls.forEach((url) => {
-        const img = new Image();
-        img.src = url;
-    });
-}
-
-// Preload after initial page load to avoid blocking core content
-window.addEventListener('load', () => {
-    preloadImages(hoverImages);
-});
